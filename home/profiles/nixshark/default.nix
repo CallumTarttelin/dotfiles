fd0bc402017911ae26307b0b825b5b198b9411d6
callumtarttelin <callum@tarttelin.co.uk>
2023-12-17 01:46:45 +0000

    Adding jetbrains IDEs

--- home/profiles/nixshark/default.nix
+++ home/profiles/nixshark/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ../../wayland
     ../../programs
+    ../../programs/jetbrains.nix
   ];
 }
---
+++ home/programs/jetbrains.nix
@@ -0,0 +1,12 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+      jetbrains.idea-ultimate
+      jetbrains.rust-rover
+      jetbrains.webstorm
+      jetbrains.pycharm-professional
+  ];
+}
