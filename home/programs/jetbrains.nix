88a94ce4f1e46e4168a2dc18d9c67d74f932e5c6
callumtarttelin <callum@tarttelin.co.uk>
2024-03-10 23:14:14 +0000

    Adding toolbox + vscode

--- home/programs/jetbrains.nix
+++ home/programs/jetbrains.nix
@@ -4,5 +4,13 @@
     jetbrains.rust-rover
     jetbrains.webstorm
     jetbrains.pycharm-professional
+    jetbrains-toolbox
   ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs; [
+      vscode-extensions.vscodevim.vim
+    ];
+  };
 }
