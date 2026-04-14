c50a395629e6a54f5085105ba2a4379c83165571
Flake Updater <flake-updater@callumtarttelin.com>
2026-04-14 19:46:42 +0100

    chore: update flake.lock 2026-04-14 (#17)

--- pkgs/t3code/default.nix
+++ pkgs/t3code/default.nix
@@ -5,11 +5,11 @@
   makeDesktopItem,
 }: let
   pname = "t3code";
-  version = "0.0.15";
+  version = "0.0.17";
 
   src = fetchurl {
     url = "https://github.com/pingdotgg/t3code/releases/download/v${version}/T3-Code-${version}-x86_64.AppImage";
-    hash = "sha256-Z8y7SWH55+ZC7cRpgo0cdG273rbDiFS3pXQt3up7sDg=";
+    hash = "sha256-uS+o1nRA3R7hn9BaomrdsGVC8UcpPFFRG3a1qGVrs8w=";
   };
 
   desktopItem = makeDesktopItem {
